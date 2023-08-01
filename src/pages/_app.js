2d376b185561559be66e0b0ac0f2a6f1cfee1cf2
Mohammed Falah <falahsss900@gmail.com>
2023-08-01 17:56:20 +0530

    toasts fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import "../pages.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Toaster duration={1000} richColors />
+      {/* <Toaster duration={1000} richColors /> */}
       <Component {...pageProps} />
     </>
   );
